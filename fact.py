44a30ebf83730c9452e41e62baa9b59a2c69bda8
Aparna876254 <aptudavekar@gmail.com>
2025-09-10 00:43:25 +0530

    Added fact.py and module.py scripts

---
+++ fact.py
@@ -0,0 +1,3 @@
+num= int(input('enter the number : '))
+res= num*(factorial(num-1))
+print('factorial of',num, 'is :',res)
---
+++ module.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+sqr=math.sqrt(num)
+log= math.log(num)
+sine= math.sin(num)
+
+print(f"Square root: {sqr}")
+print(f"logarithm {log}")
+print("Sine:",sine)
